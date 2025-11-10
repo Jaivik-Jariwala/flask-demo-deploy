634d066d4e69cd268fa8450eacfb1932b1cdb8d4
[Jaivik Jariwala] <jaivikjariwala111@gmail.com>
2025-11-10 11:58:38 +0530

    commit update check

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
     env = os.environ.get("FLASK_ENV","development")
     db_url = os.environ.get("DATABASE_URL","sqlite:///test.db")
-    return f"<h1> hey this is jaivik"
+    return f"<h1> hey this is krish"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8000)
